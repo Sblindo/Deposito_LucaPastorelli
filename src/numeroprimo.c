a633cdd0c68f7479035c6c876155563c5d48763f
root <luca.pastorelli1985@outlook.it>
2025-06-10 14:49:15 +0200

    Added comment on numeroprimo.c

--- src/numeroprimo.c
+++ src/numeroprimo.c
@@ -5,21 +5,23 @@
 #include <math.h>
 
 int main() {
-    int num, primo;
-    double radice;
+    int num, primo; //interi dove inserisco il numero fornito dall'utente ed il flag con cui traccio la primalità
+    double radice; //double dove inserisco la radice quadrata del'intero fornito dall'utente 
 
-    printf("Inserisci un numero: ");
-    scanf("%d", &num);
+    printf("Inserisci un numero: "); //stampo il testo (potrei farlo direttamente dallo scanf)
+    scanf("%d", &num); //leggo il numero intero che fornisce l'utente
 
-    radice = sqrt(num);
-    primo = 1;
+    radice = sqrt(num); //calcolo la radice quadrata del numero in oggetto
+    primo = 1; //inizializzo il flag della primalità
 
+    //ciclo per controllare che il numero fornito non abbia divisori compresi tra 2 e la sua radice quadrata
     for(int i = 2; i < radice; i++){
         if(num % i == 0){
             primo = 0;
             break;
         }
     }
+    //terminato il ciclo verifico che il flag sia rimasto a 1 e il numero inserito sia diverso da 1
     if(primo && num != 1){
         printf("Il numero inserito è un numero primo.\n");
     } else {
