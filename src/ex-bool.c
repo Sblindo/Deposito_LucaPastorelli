eae5a6532f5df0a1b09d5c8e1b877a5552cdb799
root <luca.pastorelli1985@outlook.it>
2025-06-11 12:19:02 +0200

    Added new exercise ex-bool, ex-string

---
+++ src/ex-bool.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_even(int x) {
+        return (x % 2) == 0;
+    }
+
+int main(void) {
+    
+
+    int numero = 7;
+    if (is_even(numero)) {
+        printf("%d è pari\n", numero);
+    } else {
+        printf("%d è dispari\n", numero);
+    }
+
+    // Uso diretto di true/false
+    bool flag = false;
+    printf("Flag è %s\n", flag ? "vero" : "falso");
+
+    return 0;
+}
