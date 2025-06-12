01b6400ecf15002f8a6397fa0749426c2648b71d
root <luca.pastorelli1985@outlook.it>
2025-06-12 09:52:22 +0200

    Updated exercises/exscanf_120625.c

--- exercises/exscanf_120625.c
+++ exercises/exscanf_120625.c
@@ -21,23 +21,28 @@ int main(){
     printf("\nInserisci un terzo numero: ");
     scanf("%f", &num3);
 
+    //calcolo la media
     media = (num1 + num2 + num3)/3;
 
+    //contatore per il while
     int i = 0;
 
     if(media > 5){
+        //ciclo la string citta sostituendo le minuscole con le maiuscole
         while(citta[i]){
             citta[i] = toupper(citta[i]);
             i++;
         }
     }
     else{
+        //ciclo la string citta sostituendo le maiuscole con le minuscole
        while(citta[i]){
             citta[i] = tolower(citta[i]);
             i++;
         }
     }
     
+    //stampo a video tutto
     printf("\nIl nome della tua città è %s e la media dei numeri inseriti è %.2f\n", citta,media);
     return 0;
 }
