5349c4d51688ef04488d3ce5b446161c2ec83f64
root <luca.pastorelli1985@outlook.it>
2025-06-12 11:48:38 +0200

    Updated exercises/exope_120625.c

--- exercises/exope_120625.c
+++ exercises/exope_120625.c
@@ -25,6 +25,7 @@ int main(){
     printf("Prodotto = %d\n",prod);
 
     if(!(num2 == 0)){
+        //Calcolo e stampo quoziente e modulo solo se il secondo numero è diverso da zero
         quoz = num1 / num2;
         res = num1 % num2;
 
@@ -34,10 +35,12 @@ int main(){
         printf("Quoziente e modulo non possono essere calcolati con 0 a denominatore\n");
     }
     
+    //verifico se entrambe i numeri sono maggiori di 0
     if((num1 > 0) && (num2 > 0)){
         printf("Entrambi i numeri sono > di 0\n");
     }
 
+    //verifico se almeno uno dei due numeri è pari e diverso da zero
     if((((num1 % 2) == 0) || ((num2 % 2) == 0)) && !(num2 == 0)){
         printf("Almeno uno dei due numeri è pari\n");
     }
