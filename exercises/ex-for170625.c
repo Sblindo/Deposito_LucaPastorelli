803119ef1485e1f59c07eb2da75ce1a3bb732d42
root <luca.pastorelli1985@outlook.it>
2025-06-17 12:22:29 +0200

    Added exercises/ex-for170625.c

--- exercises/ex-for170625.c
+++ exercises/ex-for170625.c
@@ -11,13 +11,16 @@ int main(){
     scanf("%d", &cont);
 
     for(int i = 0; i < cont; i++){ //ciclo for per il numero cont
+
         printf("Inserisci un numero intero tra 10 e 100: ");
         scanf("%d", &num);
 
         while(num < 10 || num > 100){ //verifico che il numero sia compreso tra 10 e 100 altrimenti lo chiedo di nuovo
+
             printf("Il numero deve essere compreso tra 10 e 100\n");
             printf("Inserisci un numero intero tra 10 e 100: ");
             scanf("%d", &num);
+            
         }
 
         if(num % 2 != 0){ //se dispari aumento il contatore dispari
@@ -25,10 +28,10 @@ int main(){
         }
     }
 
-    if(cont != 0){ //Se ho inserito almeno un numero è diverso da zero stampo sullo stdout
+    if(cont != 0 && cont > 0){ //Se ho inserito almeno un numero è diverso da zero stampo sullo stdout
         printf("Il numero di dispari inseriti è: %d\n", dispari);
     }else{
-        printf("Se non inserisci nulla non posso contare i dispari!!\n");
+        printf("Se non inserisci un intero > 0 non posso contare i dispari!!\n");
     }
     
     return 0;
