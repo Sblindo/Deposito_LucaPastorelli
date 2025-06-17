57867e7023b9b94bd321a24b16f33d1414b4c53b
root <luca.pastorelli1985@outlook.it>
2025-06-17 10:02:30 +0200

    Updated exercises/ex-while170625.c

--- exercises/ex-while170625.c
+++ exercises/ex-while170625.c
@@ -3,25 +3,35 @@
 int main(){
     int num = 1; //valorizzo a 1 perchè cosi num potrà essere usato nel while
     int somma = 0;
+    char term;
 
     while(num){
         //l'utente deve inserire un numero intero
-        printf("Inserisci un numero intero: ");
-        scanf("%d", &num);
+        printf("Inserisci un numero intero(0 per terminare): ");
 
-        //verifico se num è zero, quindi stampo somma ed esco da programma
-        if(num == 0){
+        if(scanf("%d%c", &num, &term) == 2 && term == '\n'){
+            if(num == 0){
+                break;
+            }
+        //verifico se num è positivo
+            if(num > 0){
+                somma += num;
+            }
+        }else{
+            printf("!!Puoi inserire solo interi!!\n");
             break;
         }
-        //verifico se num è positivo
-        if(num > 0){
-            somma += num;
-        }
+
+        //verifico se num è zero, quindi stampo somma ed esco da programma
+        
 
 
     }
 
     //stampo sullo stdout il valore di somma
-    printf("La somma dei numeri positivi inseriti è: %d\n", somma);
+    if(somma != 0){
+        printf("La somma dei numeri positivi inseriti è: %d\n", somma);
+    }
+    
     return 0;
 }
