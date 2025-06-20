6b0057018cfce9537f01580877fcc12787c5e0d9
root <luca.pastorelli1985@outlook.it>
2025-06-20 09:29:04 +0200

    Added exercises/finale200625.c

---
+++ exercises/ex-finale200625.c
@@ -0,0 +1,13 @@
+/*
+    Esercizio finale corso fondamenti di programmazione in C
+*/
+#include <stdio.h> //includo la libreria per la gestione di I/O
+#include <stdlib.h> //includo la libreria standard per la gestione della memoria
+
+int main(int argc, char *argv[]){
+
+
+
+    //Esco dal programma senza errori
+    return EXIT_SUCCESS;
+}
